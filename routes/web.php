ecbf2324a0f6a67a21e75496c9185709a7a14603
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-10-16 14:56:32 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => 'Indra Ajiyanto'
+    ]);
+});
+Route::get('/post', function () {
+    return view('post');
 });
