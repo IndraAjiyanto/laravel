2743f21ffddf292adb092d1173998b4077842f8d
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-10-17 12:53:25 +0700

    menambahkan tailwind

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <h1>About</h1>
-    <h2><?php echo $nama; ?></h2>
+    <h2>{{$nama}}</h2>
 </body>
 </html>
